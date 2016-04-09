54bcb1ecc9254591aa11d5ff1b7fee7c220a83d5
Carl Sandrock <carl.sandrock@gmail.com>
2016-04-09 07:26:55 +0200

    Initial commit

---
+++ samplefigure.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy
+
+x = numpy.linspace(0, 10)
+modelA = 100 - x**2
+modelB = 110 - 10*x
+ydata = modelA + numpy.random.rand(len(x))*10
+
+plt.plot(x, modelA, 'k-', label='Model A')
+plt.plot(x, modelB, 'k--', label='Model B')
+plt.plot(x, ydata, 'b.', label='Experimental')
+plt.xlabel('Time (s)')
+plt.ylabel('Height (m)')
+plt.legend()
+plt.savefig('samplefigure.pdf')
