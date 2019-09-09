c69092bfa322396fc0f25a98864f4705566e315c
keaganbester <keaganbester@gmail.com>
2019-09-09 17:46:34 +0200

    An example using the subcaption package
    An example of creating a figure with multiple subfigures and a subcaption for each subfigure

--- samplefigure.py
+++ samplefigure.py
@@ -23,3 +23,31 @@ plt.legend()
 # sometimes elements get cut off on the edges.
 plt.tight_layout()
 plt.savefig('samplefigure.pdf')
+
+#Lets investigate the effect of noise on our Model "A" and it's derivative
+for index, value  in enumerate([1.0, 10, 100]):
+    ydata = modelA + numpy.random.randn(len(x))*value
+    diffA = numpy.diff(modelA)
+    ydiff = numpy.diff(ydata)
+    dx = x[1]-x[0]
+    
+    plt.figure(figsize=(5, 4))  # note figure size in inches
+    plt.plot(x, modelA, 'k-', label='Model A')
+    plt.plot(x, modelB, 'k--', label='Model B')
+    plt.plot(x, ydata, 'b.', label='Experimental')
+    plt.xlabel('Time / s')
+    plt.ylabel('Height / m')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(('modelfigure_{}.pdf').format(index))
+    
+    plt.figure(figsize=(5, 4))  # note figure size in inches
+    plt.plot(x[:-1], diffA/dx, 'k-', label='Model A time differential ')
+    plt.plot(x[:-1], ydiff/dx, 'b.', label='Experimental time differential ')
+    plt.xlabel('Time / s')
+    plt.ylabel(r'Velocity /$m\cdot s^{_1}$')
+    plt.legend()
+    # This ensures the bounding box is correct. If we don't call tight_layout
+    # sometimes elements get cut off on the edges.
+    plt.tight_layout()
+    plt.savefig(('differentialfigure_{}.pdf').format(index))
